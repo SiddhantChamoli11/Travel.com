b13f6980f646210e9c7cfb214f295afa92b69070
SiddhantChamoli11 <145254714+SiddhantChamoli11@users.noreply.github.com>
2023-09-29 02:12:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+const scrollRevealOption={
+    distance: "50px",
+    origin: "bottom",
+    duration : 1000,
+};
+
+
+ScrollReveal().reveal(".header__container h1",scrollRevealOption);
+
+ScrollReveal().reveal(".header__container h4",{
+    scrollRevealOption, delay:500,
+
+});
+
+ScrollReveal().reveal(".header__container .btn",{
+    scrollRevealOption, delay:800,
+
+});
+
+
+
+//about container
+
+ScrollReveal().reveal(".about__container .sectio__header", scrollRevealOption);
+ScrollReveal().reveal(".about__container .section__subheader",{
+     scrollRevealOption,
+     delay: 500,
+});
+
+ScrollReveal().reveal(".about__container .about__flex",{
+    scrollRevealOption,
+    delay: 800,
+});
+
+ScrollReveal().reveal(".about__container .btn",{
+    scrollRevealOption,
+    delay: 1000,
+});
+
+
+//discover
+ScrollReveal().reveal(".discover__card",
+{
+    scrollRevealOption, interval:500,
+});
+
+ScrollReveal().reveal(".discover__card__content",
+{
+    scrollRevealOption, interval:500, delay: 200,
+});
+
+
+
+
+//blogs
+ScrollReveal().reveal(".blogs__card",
+{
+    duration:1000,
+     interval:500,
+});
+
